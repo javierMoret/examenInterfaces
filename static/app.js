06a696bde2d435a5733759a9d88388f4ae701ba5
Javier Moret Galán <javier.s257989@cesurformacion.com>
2022-12-19 11:37:08 +0100

    examen acabado

--- static/app.js
+++ static/app.js
@@ -1,3 +1,16 @@
 // JAVIER_MORET_GALAN
+
+// Ventana modal
+function showModal() {
+    document.getElementById('cuadro-modal').style.display = 'block';
+    document.getElementById('cuadro-principal').style.display = 'none';
+  }
+  
+  function closeModal() {
+    document.getElementById('cuadro-modal').style.display = 'none';
+    document.getElementById('cuadro-principal').style.display = 'block';
+  }
+
+
 //
 // Completar JS aqui
